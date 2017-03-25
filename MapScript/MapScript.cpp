4267195bcd8d577e5ae23e3c448528b9f300c003
Виктор Кохно < >
2017-03-25 04:16:41 +0300

    vers.2

--- MapScript/MapScript.cpp
+++ MapScript/MapScript.cpp
@@ -1,33 +1,25 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <fstream>
 
 int main() {
+	bool spec;
 	for (std::string str; std::getline(std::cin, str);) {
-		bool k = true, j = false;
+		spec = true;
 		for (int i = 0; i < str.length(); i++) {
 			if (str[i] > 64 && str[i] < 91) {
 				str[i] += 32;
 				std::cout << str[i];
-				k = false;
-				j = true;
+				spec = false;
 			} else if (str[i] > 96 && str[i] < 123) {
 				std::cout << str[i];
-				k = false;
-				j = true;
-			} else if (str[i] == 32 && !k) {
+				spec = false;
+			} else if (!spec) {
 				std::cout << '\t' << "1" << '\n';
-				k = true;
-				j = false;
-			} else {
-				if (!k) {
-					std::cout << '\t' << "1" << '\n';
-					k = true;
-					j = false;
-				}
+				spec = true;
 			}
 		}
-		if (j) { std::cout << '\t' << "1" << '\n'; }
+		if (!spec) { std::cout << '\t' << "1" << '\n'; }
 	}
 	return 0;
 }
